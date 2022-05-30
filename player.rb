01fe76fb8e67cf7d71548d3cee5d61eab2f873fd
JC Flores <tothenextcode@gmail.com>
2022-05-30 21:38:56 +0000

    Setup main and implement Player class

---
+++ player.rb
@@ -0,0 +1,21 @@
+class Player
+  attr_reader :name :tag
+
+  def initialize(name, tag)
+    @name = name
+    @tag = tag
+    @lives = 3
+  end
+
+  def reduce_life(number)
+    @lives = @lives - number
+  end
+
+  def life
+    "#{@lives}/3"
+  end
+
+  def dead
+    @lives == 0
+  end
+end
